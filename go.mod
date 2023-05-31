b8554677bbb80cdf580ab36c76027cb293eeb53b
Diego Diomede <DDIOMEDE@claro.com.ar>
2023-05-31 16:28:02 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/diomedd/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/diomedd/godesde0/variables"
+)
+
+func main() {
+
+	fmt.Println("Hola Mundo")
+	variables.MuestroEnteros()
+}
